76a91b53ebef5a11f7b6bb60aa4fb1c2f37d4b89
snigo <snigo.ogins@gmail.com>
2020-11-21 13:42:49 +0100

    Init branch commit

---
+++ client/src/App.tsx
@@ -0,0 +1,166 @@
+import React, { FunctionComponent, useState } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Graph from './components/Graph';
+import Home from './components/Home';
+import Search from './components/Search';
+import Navbar from './components/Navbar';
+import ArticlesList from './components/ArticlesList';
+
+import {
+  fetchGraphData,
+  removeAuthorFromGraph,
+  updateArticlesList,
+  updateAuthorData,
+} from './services/ApiClient';
+import { queryPathBuilder } from './services/apiHelpers';
+
+import './App.css';
+
+const App: FunctionComponent = () => {
+  const [authorDict, setAuthorDict] = useState({});
+  const [graphData, setGraphData] = useState({});
+  const [articleList, setArticleList] = useState([]);
+  const [selectedAuthor, setSelectedAuthor] = useState('');
+  const [selectedArticle, setSelectedArticle] = useState('');
+  const [emptySearch, setEmptySearch] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [tooLarge, setTooLarge] = useState(false);
+
+  const handleSearchForm = async (
+    title,
+    author,
+    journal,
+    abstract,
+    filters
+  ) => {
+    const [query, searchFilters] = queryPathBuilder(
+      title,
+      author,
+      journal,
+      abstract,
+      filters
+    );
+    setLoading(true);
+    try {
+      const [dict, data, metadata, articles] = await fetchGraphData(
+        query,
+        searchFilters
+      );
+      if (data.links.length > 1000) {
+        setTooLarge(true);
+        setTimeout(() => setTooLarge(false), 5000);
+      } else {
+        setAuthorDict(() => dict);
+        setGraphData((init) => {
+          return {
+            ...init,
+            ...data,
+          };
+        });
+        setArticleList(articles);
+      }
+      setLoading(false);
+      return true;
+    } catch (err) {
+      setEmptySearch(true);
+      setLoading(false);
+      setTimeout(() => setEmptySearch(false), 5000);
+      return false;
+    }
+  };
+
+  const handleQuickSearch = (author) => {
+    if (articleList.length === 0) {
+      handleSearchForm('', author, '', '');
+    } else {
+      handleGraphExpand(author);
+    }
+  };
+
+  const handleGraphExpand = async (author) => {
+    const [query, _] = queryPathBuilder('', author);
+    setLoading(true);
+    try {
+      const [dict, data, metadata, articles] = await fetchGraphData(query);
+      const [updatedDict, updatedData] = await updateAuthorData(
+        authorDict,
+        dict
+      );
+      const updatedArticles = await updateArticlesList(articleList, articles);
+      setEmptySearch(false);
+      setLoading(false);
+      if (updatedData.links.length > 1000) {
+        setTooLarge(true);
+        setTimeout(() => setTooLarge(false), 5000);
+      } else {
+        setGraphData(updatedData);
+        setAuthorDict(updatedDict);
+        setArticleList(updatedArticles);
+      }
+    } catch (err) {
+      setEmptySearch(true);
+      setLoading(false);
+      setTimeout(() => setEmptySearch(false), 5000);
+    }
+  };
+
+  const removeSelectedAuthor = (author) => {
+    setGraphData(() => removeAuthorFromGraph(graphData, author));
+    setSelectedAuthor('');
+  };
+
+  const killGraph = () => {
+    setGraphData({});
+    setAuthorDict({});
+    setArticleList([]);
+    setSelectedAuthor('');
+  };
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home handleQuickSearch={handleQuickSearch} />
+        </Route>
+        <Route exact path="/search">
+          <Search
+            handleSearchForm={handleSearchForm}
+            loading={loading}
+            setSelectedAuthor={setSelectedAuthor}
+          />
+        </Route>
+        <Route exact path="/graph">
+          <Graph
+            graphData={graphData}
+            handleGraphExpand={handleGraphExpand}
+            authorDict={authorDict}
+            selectedAuthor={selectedAuthor}
+            setSelectedAuthor={setSelectedAuthor}
+            setSelectedArticle={setSelectedArticle}
+            removeSelectedAuthor={removeSelectedAuthor}
+            handleQuickSearch={handleQuickSearch}
+            killGraph={killGraph}
+            emptySearch={emptySearch}
+            loading={loading}
+            tooLarge={tooLarge}
+            setTooLarge={setTooLarge}
+          />
+        </Route>
+        <Route exact path="/list">
+          <ArticlesList
+            authorDict={authorDict}
+            articleList={articleList}
+            selectedAuthor={selectedAuthor}
+            setSelectedAuthor={setSelectedAuthor}
+            selectedArticle={selectedArticle}
+            setSelectedArticle={setSelectedArticle}
+          />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/types/index.ts
@@ -0,0 +1,16 @@
+export interface QueryFilter {
+  cs?: boolean;
+  physics?: boolean;
+  math?: boolean;
+  eess?: boolean;
+  econ?: boolean;
+  'q-bio'?: boolean;
+  'q-fin'?: boolean;
+  stat?: boolean;
+  'date-from'?: string;
+  'date-to'?: string;
+}
+
+export interface Author {
+  
+}
